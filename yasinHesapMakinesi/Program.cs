7d9354b9aacc78e846285fef4489628b0e4cf2e8
msı <ms@YASIN>
2020-10-28 12:06:42 +0300

    First Commit

---
+++ yasinHesapMakinesi/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yasinHesapMakinesi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("İlk sayıyı giriniz : ");
+            double ilkSayi = Convert.ToDouble(Console.ReadLine());
+            Console.Write("İkinci sayıyı giriniz : ");
+            double ikinciSayi = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Hangi işlemi yapmak istiyorsunuz? +, -, /, * ");
+            string islem = Console.ReadLine();
+            
+
+            switch (islem)
+            {
+                case "+":
+                    double sonuc = ilkSayi + ikinciSayi;
+                    Console.WriteLine("İşleminizin sonucu : " + sonuc);
+                    break;
+                case "-":
+                    sonuc = ilkSayi - ikinciSayi;
+                    Console.WriteLine("İşleminizin sonucu : " + sonuc);
+                    break;
+                case "/":
+                    sonuc = ilkSayi / ikinciSayi;
+                    Console.WriteLine("İşleminizin sonucu : " + sonuc);
+                    break;
+                case "*":
+                    sonuc = ilkSayi * ikinciSayi;
+                    Console.WriteLine("İşleminizin sonucu : " + sonuc);
+                    break;
+                default:
+                    Console.WriteLine("Lütfen programı yeniden başlatıp geçerli bir işlem giriniz");
+                    break;
+            }
+            
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+  
+        }
+    }
+}
